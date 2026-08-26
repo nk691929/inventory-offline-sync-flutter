a2178d5eae872cd42ed97208f40df02791e28f21
noshadramdani <noshadprofessional69@gmail.com>
2026-08-26 08:14:42 +0500

    refactor(domain): move SyncQueueManager to domain/services; split ConnectivityService interface from implementation

--- lib/core/network/connectivity_service.dart
+++ lib/core/network/connectivity_service.dart
@@ -1,25 +1,4 @@
-import 'package:connectivity_plus/connectivity_plus.dart';
-
 abstract class ConnectivityService {
   Stream<bool> get onConnectivityChanged;
   Future<bool> get isConnected;
-}
-
-class ConnectivityServiceImpl implements ConnectivityService {
-  final Connectivity _connectivity;
-  ConnectivityServiceImpl({Connectivity? connectivity})
-    : _connectivity = connectivity ?? Connectivity();
-
-  @override
-  Future<bool> get isConnected async {
-    final results = await _connectivity.checkConnectivity();
-    return results.any((r) => r != ConnectivityResult.none);
-  }
-
-  @override
-  Stream<bool> get onConnectivityChanged {
-    return _connectivity.onConnectivityChanged.map(
-      (result) => result.any((r) => r != ConnectivityResult.none),
-    );
-  }
 }
